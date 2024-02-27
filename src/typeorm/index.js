ffa3715db12810c990e39c2769844632e589e853
kdevkh <kdevkh@gmail.com>
2024-02-27 21:21:58 +0900

    [Fix] entities fixed

--- src/typeorm/index.js
+++ src/typeorm/index.js
@@ -1,32 +1,30 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const typeorm = require('typeorm')
+const typeorm = require("typeorm");
 const dataSource = new typeorm.DataSource({
-    type: 'mysql',
+    type: "mysql",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     synchronize: true,
-    entities: [
-        require('./entity/user.entity'),
-        require('./entity/store.entity'),
-    ],
-})
+    entities: ["src/typeorm/entity/**/*.js"],
+    migrations: ["src/typeorm/migrations/**/*.js"],
+});
 
 const connectDB = () => {
     dataSource
         .initialize()
-        .then(() => console.log('mysql is successfully connected'))
-        .catch((error) => console.log(error))
-}
+        .then(() => console.log("mysql is successfully connected"))
+        .catch((error) => console.log(error));
+};
 
 const disconnectDB = () => {
     dataSource
         .destroy()
-        .then(() => console.log('mysql 연결 해제'))
-        .catch((error) => console.log(error))
-}
+        .then(() => console.log("mysql 연결 해제"))
+        .catch((error) => console.log(error));
+};
 
-module.exports = { dataSource, connectDB, disconnectDB }
+module.exports = { dataSource, connectDB, disconnectDB };
